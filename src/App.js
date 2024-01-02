f4612ba7452e2c1a91bae13f417d5b3732f2fdf5
Suyash00 <suyashhandy@gmail.com>
2024-01-02 12:06:53 +0530

    countries search app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
+  const [data, setData] = useState([]);
+  const [search, setSearch] = useState("");
+
+  const handleSearch = (e) => {
+    setSearch(e.target.value);
+  };
+
+  const filteredCountries = data.filter((item) =>
+    item.name.common.toLowerCase().includes(search.toLowerCase())
+  );
+
+  const getCountriesData = async () => {
+    try {
+      const res = await fetch("https://restcountries.com/v3.1/all");
+      const countries = await res.json();
+      setData(countries);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    getCountriesData();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="appContainer">
+      <input
+        type="text"
+        placeholder="Search for countries"
+        value={search}
+        onChange={handleSearch}
+        className="searchBar"
+      />
+
+      <div className="containerStyle">
+        {search === ""
+          ? // Display all countries if search is empty
+            data.map((item) => (
+              <div className="cardStyle" key={item.name.common}>
+                <img
+                  src={item.flags.png}
+                  alt={`flag of ${item.name.common}`}
+                  className="imagesStyle"
+                />
+                <h2>{item.name.common}</h2>
+              </div>
+            ))
+          : // Display filtered countries based on the search input
+            filteredCountries.map((item) => (
+              <div className="cardStyle" key={item.name.common}>
+                <img
+                  src={item.flags.png}
+                  alt={`flag of ${item.name.common}`}
+                  className="imagesStyle"
+                />
+                <h2>{item.name.common}</h2>
+              </div>
+            ))}
+      </div>
     </div>
   );
 }
